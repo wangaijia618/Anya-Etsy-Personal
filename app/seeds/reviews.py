21fa2668813fef9f0bb3f44e0d6b9d947ce026b1
Aijia Wang <ajw7427@gmail.com>
2023-10-18 13:31:35 -0500

    reviews seed

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -322,6 +322,13 @@ def seed_reviews():
 # TRUNCATE Removes all the data from the table, and RESET IDENTITY
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
+# def undo_reviews():
+#     db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+#     db.session.commit()
 def undo_reviews():
-    db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM reviews")
+
     db.session.commit()
